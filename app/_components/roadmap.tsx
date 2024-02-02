1b968f3f902fb628d996d291accefdb1cea340e6
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-02-02 13:49:13 +0530

    structure

--- app/_components/roadmap.tsx
+++ app/_components/roadmap.tsx
@@ -1,5 +1,10 @@
 const Roadmap = () => {
-	return <div className="h-96 bg-green-400 w-full">Roadmap</div>;
+	return (
+		<div className="h-96 bg-green-400 w-full">
+			<div>venue</div>
+			<div>Roadmap</div>
+		</div>
+	);
 };
 
 export default Roadmap;
