96a3320f10c9f7604eb7de83dcaf7fc9add47db3
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-02-02 13:46:21 +0530

    added structure of roadmap

---
+++ app/_components/roadmap.tsx
@@ -0,0 +1,5 @@
+const Roadmap = () => {
+	return <div className="h-96 bg-green-400 w-full">Roadmap</div>;
+};
+
+export default Roadmap;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,35 +3,37 @@ import "@/app/globals.css";
 import { cakra, turretRoad } from "@/public/fonts";
 import Copyright from "@/components/copyright";
 import { Connect, SocialConnect } from "@/components/connect";
+import Roadmap from "./_components/roadmap";
 
 export const metadata: Metadata = {
-  title: "Designathon '24",
-  description:
-    "Designathon conducted as a part of Tech Fest organized by Christ College of Engineering, Irinjalakuda",
+	title: "Designathon '24",
+	description:
+		"Designathon conducted as a part of Tech Fest organized by Christ College of Engineering, Irinjalakuda",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/logos/designathon-logo.svg" />
-      </head>
+	return (
+		<html lang="en">
+			<head>
+				<link rel="icon" href="/logos/designathon-logo.svg" />
+			</head>
 
-      <body
-        className={`${turretRoad.variable} ${cakra.variable} antialiased bg-dark`}
-      >
-        {children}
+			<body
+				className={`${turretRoad.variable} ${cakra.variable} antialiased bg-dark`}
+			>
+				{children}
 
-        <footer>
-          <Connect />
-          <SocialConnect />
-          <Copyright />
-        </footer>
-      </body>
-    </html>
-  );
+				<footer>
+					<Connect />
+					<SocialConnect />
+					<Roadmap />
+					<Copyright />
+				</footer>
+			</body>
+		</html>
+	);
 }
